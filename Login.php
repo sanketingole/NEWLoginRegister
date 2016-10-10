9346880c752f036d4a39ccf19b3d2f08b2129463
sanketingole <sanketingole121@gmail.com>
2016-10-10 15:26:02 +0530

    Update Login.php

--- Login.php
+++ Login.php
@@ -1,5 +1,5 @@
 <?php
-    $con = mysqli_connect("mysql10.000webhost.com", "a3288368_user", "abcd1234", "a3288368_data");
+    $con = mysqli_connect("	mysql4.000webhost.com", "	a4734462_sanket", "sanket112233", "a4734462_MP");
     
     $username = $_POST["username"];
     $password = $_POST["password"];
